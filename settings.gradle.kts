1d8c82c70240f20b98b9336f1ea57a34e2911986
testbook11 <lyly2518644@163.com>
2026-06-09 22:35:14 +0800

    修正 settings.gradle.kts 语法错误

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+
+dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "LotteryAccountBook"
 include(":app")
